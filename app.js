dc10cd0496aa716ba350976564b94b0d0825e40a
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-06-29 20:23:02 +0200

    xd

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const blogRouters = require("./controller/blog");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/api", () => {
+app.get("/", () => {
 	console.info("there's no god but Allah ");
 });
-
 app.use("/api/blogs",blogRouters);
+app.use("*",(_,res)=>{
+	res.status(404).send("404 not found");
+})
 module.exports = app;
