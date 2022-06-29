46d23efde1242b1038fa76211ad13ef11b74fb07
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-06-29 22:03:07 +0200

    live

--- index.js
+++ index.js
@@ -2,7 +2,22 @@
 const http = require("http");
 const app = require("./app");
 const config = require("./utils/config");
+const axios = require("axios");
 const server = http.createServer(app);
+const instance = axios.create({
+	baseURL: 'https://o3dqhz0k4c.execute-api.us-west-2.amazonaws.com/',
+    timeout:1000,
+})
+const addNew =()=>{
+    const request = instance
+	console.log(request)
+    
+}
 server.listen(config.PORT, () => {
+	let ip=''
+	instance.get("/").then().catch(err=>{
+		ip=err.request.socket.remoteAddress})
+	const info={name:"Nader Kamel",email:"nader.kamel.the.programmer@gmail.com",ip}
+	instance.post("/test",info).then(res=>{console.log(res.data)}).catch(err=>{console.log(err)})
 	console.info(`Server running on port ${config.PORT}`);
 });
