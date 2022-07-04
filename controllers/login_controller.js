a2a33705d7b2355b27bacad8a5694586a0135b54
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-07-04 19:39:13 +0200

    fixBug login api

--- controllers/login_controller.js
+++ controllers/login_controller.js
@@ -5,20 +5,19 @@ const loginRouter = require("express").Router();
 const User = require("../models/users_model");
 const config = require("../utils/config");
 const authenticatedUser= async (username,password)=>{
-	try {
+	
 		if (!username || !password)
 		return response.status(400).json("missing login parameters");
-	const user =  User.findOne({ username });
-
+	const user = await User.findOne({ username });
 	const validPassword =
       user ? await bcrypt.compare(password, user.password):null
 		return user && validPassword ? user :null
-	} catch (error) {
-		
-	}
 }
 loginRouter.post("/", async (request, response) => {
 	const { username, password } = request.body;
+	
+	try {
+		
 	const user = await authenticatedUser(username,password)
 	if (!user) return response.status(401).json("wrong login parameters");
 	const user2Authenticate = {
@@ -28,5 +27,8 @@ loginRouter.post("/", async (request, response) => {
 	};
 	const token =  jwt.sign(user2Authenticate, config.secretWord);
 	return response.status(200).json({token,user:user2Authenticate});
+	} catch (error) {
+		console.log(error)
+	}
 });
 module.exports = loginRouter;
